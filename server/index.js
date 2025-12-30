d09648b0f863a22ea26033a53a34d241558109d9
RelativelyBurberry <saxenaanipun@gmail.com>
2025-12-30 06:53:04 +0530

    dw#2

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL
+  ]
 }));
+
 app.use(express.json())
 
 app.post('/checkout', async (req, res) => {
@@ -33,6 +37,7 @@ app.post('/checkout', async (req, res) => {
 
 })
 
-app.listen(3000, () =>
-  console.log('Server started on port 3000')
-)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
